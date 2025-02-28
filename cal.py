78c9ebe4c89295c261afddb9a0298561012941a4
Subha Chakraborty <86897887+substobeme@users.noreply.github.com>
2025-02-28 17:02:49 +0530

    The calculator files

---
+++ cal.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+def summation(data):
+    return sum(data)
+import math
+
+def square_root(x):
+    if x < 0:
+        raise ValueError("Don't enter a negative number")
+    return math.sqrt(x)
+
+def factorial(x):
+    if x < 0:
+        raise ValueError("Don't enter a negative number")
+    return math.factorial(x)
+
+def natural_log(x):
+    if x <= 0:
+        raise ValueError("Don't enter a non-positive numbers")
+    return math.log(x)
+
+def power(x, b):
+    return x ** b
+
+def main():
+    while True:
+        print("\nScientific Calculator")
+        print("1. Square Root")
+        print("2. Factorial")
+        print("3. Natural Log(base e)")
+        print("4. Power ")
+        print("5. Exit")
+        
+        choice = input("Enter your choice: ")
+        
+        if choice == '1':
+            x = float(input("Enter a number: "))
+            print("Result:", square_root(x))
+        elif choice == '2':
+            x = int(input("Enter an integer: "))
+            print("Result:", factorial(x))
+        elif choice == '3':
+            x = float(input("Enter a number: "))
+            print("Result:", natural_log(x))
+        elif choice == '4':
+            x = float(input("Enter base number: "))
+            b = float(input("Enter exponent: "))
+            print("Result:", power(x, b))
+        elif choice == '5':
+            print("Leaving Calculator")
+            break
+        else:
+            print("Invalid choice, please try again.")
+
+if __name__ == "__main__":
+    main()
+
---
+++ test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import unittest
+import math
+from cal import square_root, factorial, natural_log, power
+
+class TestCalculator(unittest.TestCase):
+    
+    def test_square_root(self):
+        self.assertEqual(square_root(16),4)
+        with self.assertRaises(ValueError):
+            square_root(-1)
+    
+    def test_factorial(self):
+        self.assertEqual(factorial(5), 120)
+        with self.assertRaises(ValueError):
+            factorial(-3)
+    
+    def test_natural_log(self):
+        self.assertAlmostEqual(natural_log(1), 0)
+        self.assertAlmostEqual(natural_log(10), math.log(10))
+        with self.assertRaises(ValueError):
+            natural_log(0)
+        with self.assertRaises(ValueError):
+            natural_log(-5)
+    
+    def test_power(self):
+        self.assertEqual(power(2, 3), 8)
+        self.assertEqual(power(10, -1), 0.1)
+
+if __name__ == '__main__':
+    unittest.main()
+
